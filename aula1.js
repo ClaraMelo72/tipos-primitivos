b8b0c33113f94f3a2b790a9123d7e21277db2191
Clara Melo <claramelo072@gmail.com>
2024-09-16 22:40:17 +0000

    atualizando código

--- aula1.js
+++ aula1.js
@@ -1,1 +1,15 @@
+/*
+ola = "Hello World" 
+console.log(ola)
+*/
+const soma = 10+20
+
+
+
+console.log(soma)
+
+const num1 = 10
+const num2 = 20
+const calc = num1 - num2
+console.log("o resultado é" + calc)
 
