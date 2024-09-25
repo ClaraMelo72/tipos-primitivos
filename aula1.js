037e9f540482052ee2bdc629117d563bda98dfdd
ClaraMelo72 <claramelo072@gmail.com>
2024-09-25 19:44:06 -0300

    atualizando o github

--- aula1.js
+++ aula1.js
@@ -8,8 +8,12 @@ const soma = 10+20
 
 console.log(soma)
 
-const num1 = 10
-const num2 = 20
+const num1 = 14
+
+const num2 = 30
+
+
 const calc = num1 - num2
+
 console.log("o resultado é" + calc)
 
